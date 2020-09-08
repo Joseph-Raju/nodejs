aedd14d094667547ad893ad94a3fdb9a9fa85e9d
Joseph-Raju <josephraju1998@gmail.com>
2020-09-08 15:45:02 +0530

    ..

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 const http =require('http');
 
+
+
 /*function check(req,res){
     console.log(req);
 
 }
 http.createServer(check())*/
-
-const server = http.createServer((req,res) => {
-    console.log(req.url,req.method,req.headers);
+// console.log(req.url,req.method,req.headers);
     //process.exit();
-    res.setHeader('content-type','text/html');
-    res.write('<html>');
-    res.write('<head><title>My first Page</title></head>');
-    res.write('<body><h1>hello welcome to the page</h1> </body>');
-    res.write('</html>');
-});
+    
+const route=require('./route.js');
+
+
+const server = http.createServer(route.handler);
+ 
 
 server.listen(3000);
